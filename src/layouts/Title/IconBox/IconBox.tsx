a4b21689df810d6a18c622a53ca09a976d37cffc
Karan Singharia <ksingharia007@gmail.com>
2023-02-06 13:52:09 +0530

    Home

---
+++ src/layouts/Title/IconBox/IconBox.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  SupportAgent,
+  Newspaper,
+  QuestionAnswer,
+  PeopleAlt,
+} from "@mui/icons-material";
+import "../IconBox/IconBox.css";
+
+// import { Card, CardContent, Typography, CardActions } from "@mui/material";
+const IconBox = () => {
+  const rows = [
+    {
+      img: <SupportAgent />,
+      title: "24/7 Support",
+      disc: "Got a problem? Just get in touch. Our support team is available 24/7.",
+    },
+    {
+      img: <Newspaper />,
+      title: "Binance Blog",
+      disc: "News and updates from the world’s leading cryptocurrency exchange.",
+    },
+    {
+      img: <QuestionAnswer />,
+      title: "Community",
+      disc: "Binance is global. Join the discussion in our worldwide communities.",
+    },
+    {
+      img: <PeopleAlt />,
+      title: "Careers",
+      disc: "Help build the future of technology. Start your new career at Binance.",
+    },
+  ];
+  return (
+    <div className="icon-box" style={{ marginTop: "30px" }}>
+      <div className="items">
+        {rows.map((item) => (
+          <ul>
+            <li>{item.img}</li>
+            <li>{item.title}</li>
+            <li className="disc-sm">{item.disc}</li>
+          </ul>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default IconBox;
